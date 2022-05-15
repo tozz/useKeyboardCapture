5b269ad15bb2f00815fc41d567f91ba1c4386a84
Erik Johansson <github@tozz.org>
2022-05-15 21:47:57 +0200

    Remove more template files

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = (ctx) => ({
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: ctx.env === 'production' ? { preset: ['default', { discardComments: { removeAll: true } }] } : false,
-  },
-});
